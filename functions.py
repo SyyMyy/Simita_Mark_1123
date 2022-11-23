2d7b2c0b5c496239f6f42742a581829b531e77e6
Simita Márk <simita.mark@students.jedlik.eu>
2022-11-23 10:31:59 +0100

    beolvasás

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 from os import system
+from data import *
+
+filename='jarmuvek.csv'
 
 def menu():
     system('cls')
@@ -9,4 +12,15 @@ def menu():
     print('3-Szabad helyek száma')
     print('4-Páncélozott csapatszállítók száma')
     print('5-Tankok száma')
+    print('6-Teljes hangár tartalma')
     return input('Választás: ')
+
+def beolvasas():
+    file=open(filename, 'r', encoding='utf-8')
+    file.readline()
+    for jarmu in file:
+        darabolt=jarmu.strip().split(';')
+        jarmuvek.append(darabolt[0])
+        meret.append(int(darabolt[1]))
+    file.close()    
+        
